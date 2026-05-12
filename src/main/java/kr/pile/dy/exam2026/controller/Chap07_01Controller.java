9c1a86693b955a35ef822c2730a894206d2b0675
songwooyu <songwooyu@gmail.com>
2026-05-12 09:46:53 +0900

    7주차 파일 업로드 실습 예제 2, 3

--- src/main/java/kr/pile/dy/exam2026/controller/Chap07_01Controller.java
+++ src/main/java/kr/pile/dy/exam2026/controller/Chap07_01Controller.java
@@ -1,10 +1,9 @@
 package kr.pile.dy.exam2026.controller;
 
+import kr.pile.dy.exam2026.domain.Member;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
@@ -20,9 +19,11 @@ public class Chap07_01Controller {
     }
 
     @PostMapping("/form")
-    public String RequestFileUploadResult(MultipartHttpServletRequest request, Model model) throws IOException {
-        String name = request.getParameter("name");
-        MultipartFile file = request.getFile("fileImage");
+    public String RequestFileUploadResult(@ModelAttribute Member member, Model model) throws IOException {
+//    public String RequestFileUploadResult(MultipartHttpServletRequest request, Model model) throws IOException {
+//    public String RequestFileUploadResult(@RequestParam("name")String name, @RequestParam("fileImage")MultipartFile file, Model model) throws IOException {
+        String name = member.getName();
+        MultipartFile file = member.getFileImage();
         String originFileName = file.getOriginalFilename();
         File saveFile = new File("d:\\upload\\" + name + "_" + originFileName);
 
@@ -38,5 +39,25 @@ public class Chap07_01Controller {
 
         return "viewFilePageResult";
     }
+
+//    @PostMapping("/form")
+//    public String RequestFileUploadResult(@RequestParam("name")String name, @RequestParam("fileImage")MultipartFile file, Model model) throws IOException {
+////    public String RequestFileUploadResult(MultipartHttpServletRequest request, Model model) throws IOException {
+//
+//        String originFileName = file.getOriginalFilename();
+//        File saveFile = new File("d:\\upload\\" + name + "_" + originFileName);
+//
+//        try {
+//            file.transferTo(saveFile);
+//            model.addAttribute("title", "파일업로드 결과 페이지");
+//            model.addAttribute("originFileName", originFileName);
+//            model.addAttribute("saveFileName", saveFile.getName());
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        return "viewFilePageResult";
+//    }
 
 }
--- src/main/java/kr/pile/dy/exam2026/domain/Member.java
+++ src/main/java/kr/pile/dy/exam2026/domain/Member.java
@@ -1,15 +1,17 @@
 package kr.pile.dy.exam2026.domain;
 
 import lombok.Data;
+import org.springframework.web.multipart.MultipartFile;
 
 @Data
 public class Member {
-    private String id;
-    private String passwd;
-    private String city;
-    private String gender;
-    private String[] hobby;
-    private String greetings;
-
+//    private String id;
+//    private String passwd;
+//    private String city;
+//    private String gender;
+//    private String[] hobby;
+//    private String greetings;
+    private String name;
+    private MultipartFile FileImage;
 }
 
